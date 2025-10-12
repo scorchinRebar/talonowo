97894901ba7a35a8e8c2e4f58395a8c4415def48
majomajo112 <patryk.pinczynski@o2.pl>
2025-10-12 20:32:47 +0200

    poprawka błędów

--- script.js
+++ script.js
@@ -1,89 +1,93 @@
+let currentNav = 'nav1';
+
 function openSite(nav) {
 
-  let currentNav = nav.id;
+  if (currentNav === nav.id) {
+    return;
+  }
 
-    let slider = document.getElementById('slider');
-    console.log(nav.id);
+  let slider = document.getElementById('slider');
+  console.log(nav.id);
 
-    switch (nav.id) {
-        case 'nav1':
-          slider.style.clipPath = 'inset(0% 73% 90% 20%)'
-                fetch ('zasady.html')
-                .then(response => response.text())
-                .then(data => {
-                  document.getElementById('slider').innerHTML = data;
+  switch (nav.id) {
+    case 'nav1':
+      currentNav = 'nav1'
+      slider.style.clipPath = 'inset(0% 73% 90% 20%)'
+        fetch ('zasady.html')
+          .then(response => response.text())
+          .then(data => {
+            document.getElementById('slider').innerHTML = data;
 
-                    slider.style.transitionDuration = '1s';
-                    slider.style.transform = 'translateY(17.6%)'
-                    slider.style.clipPath = 'inset(0% 0% 0% 0%)'
+            slider.style.transitionDuration = '1s';
+            slider.style.transform = 'translateY(17.6%)'
+            slider.style.clipPath = 'inset(0% 0% 0% 0%)'
 
-                    slider.addEventListener('transitionend', () => {
-                      slider.style.transitionDuration = '0s';
-                      slider.style.transform = 'translateY(-30%)';
-                      slider.style.clipPath = 'inset(0% 73% 90% 20%)'
-                      document.getElementById('mainContent').innerHTML = data;
-                      document.getElementById('slider').innerHTML = '';
-                    });
-                    
-                });
-            break;
+            slider.addEventListener('transitionend', () => {
+              slider.style.transitionDuration = '0s';
+              slider.style.transform = 'translateY(-30%)';
+              slider.style.clipPath = 'inset(0% 73% 90% 20%)'
+              document.getElementById('mainContent').innerHTML = data;
+              document.getElementById('slider').innerHTML = '';
+            });
+        });
+        break;
 
-        case 'nav2':
-            // window.location.href = '';
-            break;
+    case 'nav2':
+        // window.location.href = '';
+        break;
 
-        case 'nav3':
-          slider.style.clipPath = 'inset(0% 46% 90% 46%)'
-            fetch ('kalkulator.html')
-                .then(response => response.text())
-                .then(data => {
-                  document.getElementById('slider').innerHTML = data;
+    case 'nav3':
+      currentNav = 'nav3'
+      slider.style.clipPath = 'inset(0% 46% 90% 46%)'
+        fetch ('kalkulator.html')
+          .then(response => response.text())
+          .then(data => {
+            document.getElementById('slider').innerHTML = data;
 
-                    slider.style.transitionDuration = '1s';
-                    slider.style.transform = 'translateY(17.6%)'
-                    slider.style.clipPath = 'inset(0% 0% 0% 0%)'
+            slider.style.transitionDuration = '1s';
+            slider.style.transform = 'translateY(17.6%)'
+            slider.style.clipPath = 'inset(0% 0% 0% 0%)'
 
-                    slider.addEventListener('transitionend', () => {
-                      slider.style.transitionDuration = '0s';
-                      slider.style.transform = 'translateY(-30%)';
-                      slider.style.clipPath = 'inset(0% 46% 90% 46%)'
-                      document.getElementById('mainContent').innerHTML = data;
-                      document.getElementById('slider').innerHTML = '';
-                    });
-                    
-                });
-            break;
+            slider.addEventListener('transitionend', () => {
+              slider.style.transitionDuration = '0s';
+              slider.style.transform = 'translateY(-30%)';
+              slider.style.clipPath = 'inset(0% 73% 90% 20%)'
+              document.getElementById('mainContent').innerHTML = data;
+              document.getElementById('slider').innerHTML = '';
+            });
+        });
+        break;
 
-        case 'nav4':
-            // window.location.href = '';
-            break;
+    case 'nav4':
+        // window.location.href = '';
+        break;
 
-        case 'nav5':
-          slider.style.clipPath = 'inset(0% 20% 90% 73%)'
-            fetch ('autorzy.html')
-                .then(response => response.text())
-                .then(data => {
-                  document.getElementById('slider').innerHTML = data;
+    case 'nav5':
+      currentNav = 'nav5'
+      slider.style.clipPath = 'inset(0% 20% 90% 73%)'
+        fetch ('autorzy.html')
+          .then(response => response.text())
+          .then(data => {
+            document.getElementById('slider').innerHTML = data;
 
-                    slider.style.transitionDuration = '1s';
-                    slider.style.transform = 'translateY(17.6%)'
-                    slider.style.clipPath = 'inset(0% 0% 0% 0%)'
+            slider.style.transitionDuration = '1s';
+            slider.style.transform = 'translateY(17.6%)'
+            slider.style.clipPath = 'inset(0% 0% 0% 0%)'
 
-                    slider.addEventListener('transitionend', () => {
-                      slider.style.transitionDuration = '0s';
-                      slider.style.transform = 'translateY(-30%)';
-                      slider.style.clipPath = 'inset(0% 20% 90% 73%)'
-                      document.getElementById('mainContent').innerHTML = data;
-                      document.getElementById('slider').innerHTML = '';
-                    });
-                    
-                });
-            break;
-        
-        default:
-            console.log('przekazano zły nav.id');
-            break;
-    }
+            slider.addEventListener('transitionend', () => {
+              slider.style.transitionDuration = '0s';
+              slider.style.transform = 'translateY(-30%)';
+              slider.style.clipPath = 'inset(0% 73% 90% 20%)'
+              document.getElementById('mainContent').innerHTML = data;
+              document.getElementById('slider').innerHTML = '';
+            });
+        });
+        break;
+    
+    default:
+        console.log('przekazano zły nav.id');
+        break;
+  }
 }
 
 function przelicz() {
@@ -109,3 +113,4 @@ function przelicz() {
 
     poleWyniku.textContent = `${ilosc} ${walutaZ} = ${wynik} ${walutaNa}`;
 }
+
